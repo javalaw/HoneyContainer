35c67ddfe1708ca8a54a562c0defadd7c5adf1e4
刘志为 <739554820@qq.com>
2018-06-05 00:21:46 +0800

    init commit

---
+++ src/Honey/Container.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * the service container of honey
+ */
+namespace Honey;
+
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class Container implements ContainerInterface
+{
+
+    protected static $services = [];
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
+     * @throws ContainerExceptionInterface Error while retrieving the entry.
+     *
+     * @return mixed Entry.
+     */
+    public function get($id)
+    {
+        if (!$this->exists($id)){
+            //todo: throw not found exception
+        }
+        return self::$services[$id];
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * `has($id)` returning true does not mean that `get($id)` will not throw an exception.
+     * It does however mean that `get($id)` will not throw a `NotFoundExceptionInterface`.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return bool
+     */
+    public function has($id):bool
+    {
+        return $this->exists($id);
+    }
+
+    /**
+     * @param string $id
+     * @return bool
+     */
+    public function exists(string $id):bool
+    {
+        return isset(static::$services[$id]);
+    }
+}
